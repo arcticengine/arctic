631100ee10c62ceb7469ae846eb30160304560d2
Huldra <povelitel2003@gmail.com>
2017-05-28 03:50:34 +0300

    Implement random maze generation, fix tile size

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -23,6 +23,7 @@
 // demo.cpp : Defines the entry point for the application.
 #include "demo/demo.h"
 
+#include <time.h>
 #include "engine/easy.h"
 
 using namespace arctic;
@@ -33,28 +34,48 @@ Sprite g_hero;
 Sprite g_floor;
 Sound g_step;
 
-Si32 g_maze[16][10];
+Si32 g_maze[32][20];
 Vec2Si32 g_hero_pos(1, 1);
 
+void StepMazeGeneration(Vec2Si32 from, Vec2Si32 size) {
+    g_maze[from.x][from.y] = 1;
+    Vec2Si32 direction[4] = {Vec2Si32(-1, 0)
+        , Vec2Si32(1, 0)
+        , Vec2Si32(0, -1)
+        , Vec2Si32(0, 1)};
+    for (Si32 variants = 4; variants > 0; --variants) {
+        Si32 idx = rand() % variants;
+        Vec2Si32 dir = direction[idx];
+        Vec2Si32 path = from + dir;
+        Vec2Si32 to = path + dir;
+        if (to.x > 0 && to.x < size.x - 1 && to.y > 0 && to.y < size.y - 1) {
+            if (g_maze[to.x][to.y] == 0) {
+                g_maze[path.x][path.y] = 1;
+                StepMazeGeneration(to, size);
+            }
+        }
+        direction[idx] = direction[variants - 1];
+    }
+}
+
 void Init() {
-    ResizeScreen(320, 200);
+    ResizeScreen(800, 500);
 
     g_wall.Load("data/wall1.tga");
     g_hero.Load("data/hero1.tga");
     g_floor.Load("data/floor1.tga");
     g_step.Load("data/step.wav");
 
-    for (Si32 x = 0; x < 16; ++x) {
-        for (Si32 y = 0; y < 10; ++y) {
-            if (x == 0 || x == 15 || y == 0 || y == 9) {
-                g_maze[x][y] = 0;
-            } else {
-                g_maze[x][y] = 1;
-            }
+    Vec2Si32 maze_size(32, 20);
+    for (Si32 x = 0; x < maze_size.x; ++x) {
+        for (Si32 y = 0; y < maze_size.y; ++y) {
+            g_maze[x][y] = 0;
         }
     }
 
     g_hero_pos = Vec2Si32(1, 1);
+    srand(static_cast<unsigned int>(time(nullptr)));
+    StepMazeGeneration(g_hero_pos, maze_size);
 }
 
 void Update() {
@@ -81,15 +102,15 @@ void Update() {
 }
 
 void Render() {
-    for (Si32 x = 0; x < 16; ++x) {
-        for (Si32 y = 9; y >= 0; --y) {
+    for (Si32 x = 0; x < 32; ++x) {
+        for (Si32 y = 19; y >= 0; --y) {
             if (g_maze[x][y] == 0) {
-                g_wall.Draw(x * 20, y * 20);
+                g_wall.Draw(x * 25, y * 25);
             } else {
-                g_floor.Draw(x * 20, y * 20);
+                g_floor.Draw(x * 25, y * 25);
             }
             if (x == g_hero_pos.x && y == g_hero_pos.y) {
-                g_hero.Draw(x * 20 + 2, y * 20 + 10);
+                g_hero.Draw(x * 25 + 5, y * 25 + 10);
             }
         }
     }
