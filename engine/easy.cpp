aebfe431916a8f4806dc99e6a62b702b5ebc9db8
Huldra <povelitel2003@gmail.com>
2017-06-23 03:21:20 +0300

    Implement DrawTriangle

--- engine/easy.cpp
+++ engine/easy.cpp
@@ -26,6 +26,7 @@
 #include <chrono>  // NOLINT
 #include <limits>
 #include <thread>  // NOLINT
+#include <utility>
 
 #include "engine/arctic_platform.h"
 
@@ -222,11 +223,196 @@ void DrawTriangle(Vec2Si32 a, Vec2Si32 b, Vec2Si32 c, Rgba color) {
     DrawTriangle(a, b, c, color, color, color);
 }
 
+// Just like MasterBoy wrote in HUGi 17, but without subpixel
+// see http://www.hugi.scene.org/online/coding/hugi%2017%20-%20cotriang.htm
+// or http://www.hugi.scene.org/online/hugi17/
+inline void DrawTrianglePart(Rgba *dst, Si32 stride,
+        float *x1, float *x2, Vec4F *rgba_a, Vec4F *rgba_b,
+        float dxdy1, float dxdy2,
+        Vec4F dcdy1, Vec4F dcdy2,
+        Si32 width, Si32 height,
+        Si32 y1, Si32 y2) {
+    Si32 y = y1;
+    if (y1 < 0) {
+        Si32 yc = std::min(0, y2);
+        float d = static_cast<float>(yc - y);
+        y = yc;
+        *x1 += dxdy1 * d;
+        *x2 += dxdy2 * d;
+        *rgba_a += dcdy1 * d;
+        *rgba_b += dcdy2 * d;
+    }
+    Si32 ye = std::min(height, y2);
+    dst += y * stride;
+    for (; y < ye; y++) {
+        Si32 x1i = static_cast<Si32>(*x1);
+        Si32 x2i = static_cast<Si32>(*x2);
+
+        Si32 x12i = x2i - x1i;
+        Si32 x1c = std::max(0, x1i);
+        Si32 x2c = std::min(width, x2i);
+
+        if (x2c <= x1c) {
+            if (x2c == x1c && x2c < width) {
+                Rgba color(Ui8(rgba_a->x),
+                    Ui8(rgba_a->y),
+                    Ui8(rgba_a->z),
+                    Ui8(rgba_a->w));
+                Rgba *p = dst + x1c;
+                p->rgba = color.rgba;
+            }
+        } else {
+            Vec4F rgba_ab = *rgba_b - *rgba_a;
+            Vec4F rgba_1c = *rgba_a + rgba_ab * static_cast<float>(x1c - x1i) /
+                static_cast<float>(x12i);
+            Vec4F rgba_2c = *rgba_a + rgba_ab * static_cast<float>(x2c - x1i) /
+                static_cast<float>(x12i);
+            Vec4F rgba_1c2c = rgba_2c - rgba_1c;
+
+            Vec4Si32 rgba_16(rgba_1c * 65536.f);
+            Vec4Si32 rgba_12_16(rgba_1c2c * 65536.f);
+            Vec4Si32 rgba_12_16_step = rgba_12_16 / (x2c - x1c);
+
+            Rgba *p = dst + x1c;
+            for (Si32 x = x1c; x < x2c; ++x) {
+                Rgba color(
+                    rgba_16.x >> 16,
+                    rgba_16.y >> 16,
+                    rgba_16.z >> 16,
+                    rgba_16.w >> 16);
+                p->rgba = color.rgba;
+                p++;
+                rgba_16 += rgba_12_16_step;
+            }
+        }
+        *x1 += dxdy1;
+        *x2 += dxdy2;
+        *rgba_a += dcdy1;
+        *rgba_b += dcdy2;
+        dst += stride;
+    }
+}
+
 void DrawTriangle(Vec2Si32 a, Vec2Si32 b, Vec2Si32 c,
-        Rgba color_a, Rgba color_b, Rgba color_c) {
-    DrawLine(a, b, color_a, color_b);
-    DrawLine(a, c, color_a, color_c);
-    DrawLine(b, c, color_b, color_c);
+    Rgba color_a, Rgba color_b, Rgba color_c) {
+    if (a.y > b.y) {
+        std::swap(a, b);
+        std::swap(color_a, color_b);
+    }
+    if (a.y > c.y) {
+        std::swap(a, c);
+        std::swap(color_a, color_c);
+    }
+    if (b.y > c.y) {
+        std::swap(b, c);
+        std::swap(color_b, color_c);
+    }
+    if (a.y == c.y) {
+        return;
+    }
+    Sprite back = GetEngine()->GetBackbuffer();
+    Si32 stride = back.StridePixels();
+    Rgba *dst = back.RgbaData();
+    Si32 width = back.width();
+    Si32 height = back.height();
+
+    float dxdy_ac = static_cast<float>(c.x - a.x) /
+        static_cast<float>(c.y - a.y);
+    float dxdy_bc = static_cast<float>(c.x - b.x) /
+        static_cast<float>(c.y - b.y);
+    float dxdy_ab = static_cast<float>(b.x - a.x) /
+        static_cast<float>(b.y - a.y);
+
+    Vec4F rgba_a(color_a.r, color_a.g, color_a.b, color_a.a);
+    Vec4F rgba_b(color_b.r, color_b.g, color_b.b, color_b.a);
+    Vec4F rgba_c(color_c.r, color_c.g, color_c.b, color_c.a);
+
+    Vec4F dcdy_ac = (rgba_c - rgba_a) / static_cast<float>(c.y - a.y);
+    Vec4F dcdy_bc = (rgba_c - rgba_b) / static_cast<float>(c.y - b.y);
+    Vec4F dcdy_ab = (rgba_b - rgba_a) / static_cast<float>(b.y - a.y);
+
+    float x1;
+    float x2;
+    float dxdy1;
+    float dxdy2;
+    Vec4F rgba1;
+    Vec4F rgba2;
+    Vec4F dcdy1;
+    Vec4F dcdy2;
+
+    bool is_b_at_the_right_side = dxdy_ac < dxdy_ab;
+    if (is_b_at_the_right_side) {
+        dxdy1 = dxdy_ac;
+        dcdy1 = dcdy_ac;
+        if (a.y == b.y) {
+            dxdy2 = dxdy_bc;
+            x1 = static_cast<float>(a.x);
+            x2 = static_cast<float>(b.x);
+            dcdy2 = dcdy_bc;
+            rgba1 = rgba_a;
+            rgba2 = rgba_b;
+            DrawTrianglePart(dst, stride, &x1, &x2, &rgba1, &rgba2,
+                dxdy1, dxdy2,
+                dcdy1, dcdy2, width, height, a.y, c.y);
+            return;
+        }
+        if (a.y < b.y) {
+            dxdy2 = dxdy_ab;
+            x1 = static_cast<float>(a.x);
+            x2 = static_cast<float>(a.x);
+            dcdy2 = dcdy_ab;
+            rgba1 = rgba_a;
+            rgba2 = rgba_a;
+            DrawTrianglePart(dst, stride, &x1, &x2, &rgba1, &rgba2,
+                dxdy1, dxdy2,
+                dcdy1, dcdy2, width, height, a.y, b.y);
+        }
+        if (b.y < c.y) {
+            dxdy2 = dxdy_bc;
+            x2 = static_cast<float>(b.x);
+            dcdy2 = dcdy_bc;
+            rgba2 = rgba_b;
+            DrawTrianglePart(dst, stride, &x1, &x2, &rgba1, &rgba2,
+                dxdy1, dxdy2,
+                dcdy1, dcdy2, width, height, b.y, c.y);
+        }
+    } else {
+        // b is at the left side
+        dxdy2 = dxdy_ac;
+        dcdy2 = dcdy_ac;
+        if (a.y == b.y) {
+            dxdy1 = dxdy_bc;
+            x1 = static_cast<float>(b.x);
+            x2 = static_cast<float>(a.x);
+            dcdy1 = dcdy_bc;
+            rgba1 = rgba_b;
+            rgba2 = rgba_a;
+            DrawTrianglePart(dst, stride, &x1, &x2, &rgba1, &rgba2,
+                dxdy1, dxdy2,
+                dcdy1, dcdy2, width, height, a.y, c.y);
+            return;
+        }
+        if (a.y < b.y) {
+            dxdy1 = dxdy_ab;
+            x1 = static_cast<float>(a.x);
+            x2 = static_cast<float>(a.x);
+            dcdy1 = dcdy_ab;
+            rgba1 = rgba_a;
+            rgba2 = rgba_a;
+            DrawTrianglePart(dst, stride, &x1, &x2, &rgba1, &rgba2,
+                dxdy1, dxdy2,
+                dcdy1, dcdy2, width, height, a.y, b.y);
+        }
+        if (b.y < c.y) {
+            dxdy1 = dxdy_bc;
+            x1 = static_cast<float>(b.x);
+            dcdy1 = dcdy_bc;
+            rgba1 = rgba_b;
+            DrawTrianglePart(dst, stride, &x1, &x2, &rgba1, &rgba2,
+                dxdy1, dxdy2,
+                dcdy1, dcdy2, width, height, b.y, c.y);
+        }
+    }
 }
 
 void ShowFrame() {
@@ -311,7 +497,7 @@ double Time() {
 }
 
 void Sleep(double duration_seconds) {
-	SleepSeconds(duration_seconds);
+    SleepSeconds(duration_seconds);
 /*    if (duration_seconds <= 0.0) {
         return;
     }
