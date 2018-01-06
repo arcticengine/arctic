0e03cf5dbebfb8e9284ee8bccbd4e6cfcb6a5f33
Huldra <povelitel2003@gmail.com>
2018-01-06 04:25:18 +0300

    Update cleanup script

--- cleanup.bat
+++ cleanup.bat
@@ -2,7 +2,12 @@ rmdir ipch /S /Q
 rmdir Debug /S /Q
 rmdir Release /S /Q
 rmdir x64 /S /Q
+rmdir build /S /Q
+rmdir intermediate /S /Q
 rmdir demo\Debug /S /Q
 rmdir demo\Release /S /Q
 rmdir demo\x64 /S /Q
+rmdir fjortris\Debug /S /Q
+rmdir fjortris\Release /S /Q
+rmdir fjortris\x64 /S /Q
 del demo.VC.db
