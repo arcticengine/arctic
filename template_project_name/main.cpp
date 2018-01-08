3755217db53b481c632bbc9d1dc0b38abe1d7577
Huldra <arcticengine@gmail.com>
2018-01-08 03:18:29 +0300

    Implement Snow Wizard (work in progress)

--- template_project_name/main.cpp
+++ template_project_name/main.cpp
@@ -1,6 +1,6 @@
 // The MIT License(MIT)
 //
-// Copyright 2016-2017 Huldra
+// Copyright 2016-2018 Huldra
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files(the "Software"), to deal
@@ -21,198 +21,267 @@
 // IN THE SOFTWARE.
 
 // fjortris.cpp : Defines the entry point for the application.
-#include "fjortris/fjortris.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "engine/easy.h"
 
 using namespace arctic;  // NOLINT
 using namespace arctic::easy;  // NOLINT
 
-Sprite g_blocks[3];
-Si32 g_field[16][8] = {
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0},
-	{2,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,2,2},
-	{2,0,0,0,0,2,2,2},
-	{2,0,0,0,2,2,2,2},
-	{2,0,0,0,2,2,2,2},
-	{2,0,0,0,2,2,2,2},
-};
-Si32 g_tetraminoes[5][5 * 7] = {
-	{0,0,1,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0},
-	{0,0,1,0,0,  0,0,1,0,0,  0,0,1,0,0,  0,0,0,0,0,  0,0,1,1,0,  0,1,1,0,0,  0,0,1,1,0},
-	{0,0,1,0,0,  0,0,1,0,0,  0,0,1,0,0,  0,1,1,1,0,  0,1,1,0,0,  0,0,1,1,0,  0,0,1,1,0},
-	{0,0,1,0,0,  0,0,1,1,0,  0,1,1,0,0,  0,0,1,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0},
-	{0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0,  0,0,0,0,0},
-};
-Si32 g_current[4][5][5];
-Si32 g_current_x;
-Si32 g_current_y;
-Si32 g_current_orientation;
-double g_prev_time;
-Si32 g_dx = 0;
-Si32 g_dy = 0;
-Si32 g_rotate = 0;
-Si32 g_drop = 0;
-Si32 g_score = 0;
 Font g_font;
-void StartNewTetramino() {
-	Si32 idx = Random32(0, 6);
-	for (Si32 y = 0; y < 5; ++y) {
-		for (Si32 x = 0; x < 5; ++x) {
-			g_current[0][y][x] = g_tetraminoes[y][x + idx * 5];
-			g_current[1][x][4 - y] = g_tetraminoes[y][x + idx * 5];
-			g_current[2][4 - y][4 - x] = g_tetraminoes[y][x + idx * 5];
-			g_current[3][4 - x][y] = g_tetraminoes[y][x + idx * 5];
-		}
-	}
-	g_current_x = 3;
-	g_current_y = 0;
-	g_current_orientation = Random32(0, 3);
+std::string g_project_name;
+std::string g_progress;
+
+/*void ReplaceAll(std::string &str, const std::string &from,
+    const std::string &to) {
+  if (from.empty()) {
+    return;
+  }
+  size_t start_pos = 0;
+  while (true) {
+    start_pos = str.find(from, start_pos));
+    if (start_pos == std::string::npos) {
+      return;
+    }
+    str.replace(start_pos, from.length(), to);
+    start_pos += to.length();
+  }
+}*/
+
+void UpdateResolution() {
+  Vec2Si32 size = WindowSize();
+  while (size.x > 1600 && size.y > 1000) {
+    size = size / 2;
+  }
+  if (ScreenSize() != size) {
+    ResizeScreen(size);
+  }
 }
-void ClearField() {
-	for (Si32 y = 0; y < 16; ++y) {
-		for (Si32 x = 0; x < 8; ++x) {
-		    g_field[y][x] = 0;
-		}
-	}
+
+bool GetProjectName() {
+  while (!IsKeyDownward(kKeyEscape)) {
+    UpdateResolution();
+    Clear();
+    for (Si32 idx = 0; idx < InputMessageCount(); ++idx) {
+      const InputMessage &m = GetInputMessage(idx);
+      const char *ch = "";
+      bool is_backspace = false;
+      bool is_enter = false;
+      if (m.kind == InputMessage::kKeyboard && m.keyboard.key_state == 1) {
+        switch (m.keyboard.key) {
+          case kKey0:
+          case kKeyNumpad0:
+            ch = "0";
+            break;
+          case kKey1:
+          case kKeyNumpad1:
+            ch = "1";
+            break;
+          case kKey2:
+          case kKeyNumpad2:
+            ch = "2";
+            break;
+          case kKey3:
+          case kKeyNumpad3:
+            ch = "3";
+            break;
+          case kKey4:
+          case kKeyNumpad4:
+            ch = "4";
+            break;
+          case kKey5:
+          case kKeyNumpad5:
+            ch = "5";
+            break;
+          case kKey6:
+          case kKeyNumpad6:
+            ch = "6";
+            break;
+          case kKey7:
+          case kKeyNumpad7:
+            ch = "7";
+            break;
+          case kKey8:
+          case kKeyNumpad8:
+            ch = "8";
+            break;
+          case kKey9:
+          case kKeyNumpad9:
+            ch = "9";
+            break;
+          case kKeyA:
+            ch = "a";
+            break;
+          case kKeyB:
+            ch = "b";
+            break;
+          case kKeyC:
+            ch = "c";
+            break;
+          case kKeyD:
+            ch = "d";
+            break;
+          case kKeyE:
+            ch = "e";
+            break;
+          case kKeyF:
+            ch = "f";
+            break;
+          case kKeyG:
+            ch = "g";
+            break;
+          case kKeyH:
+            ch = "h";
+            break;
+          case kKeyI:
+            ch = "i";
+            break;
+          case kKeyJ:
+            ch = "j";
+            break;
+          case kKeyK:
+            ch = "k";
+            break;
+          case kKeyL:
+            ch = "l";
+            break;
+          case kKeyM:
+            ch = "m";
+            break;
+          case kKeyN:
+            ch = "n";
+            break;
+          case kKeyO:
+            ch = "o";
+            break;
+          case kKeyP:
+            ch = "p";
+            break;
+          case kKeyQ:
+            ch = "q";
+            break;
+          case kKeyR:
+            ch = "r";
+            break;
+          case kKeyS:
+            ch = "s";
+            break;
+          case kKeyT:
+            ch = "t";
+            break;
+          case kKeyU:
+            ch = "u";
+            break;
+          case kKeyV:
+            ch = "v";
+            break;
+          case kKeyW:
+            ch = "w";
+            break;
+          case kKeyX:
+            ch = "x";
+            break;
+          case kKeyY:
+            ch = "y";
+            break;
+          case kKeyZ:
+            ch = "z";
+            break;
+          case kKeyMinus:
+            ch = "_";
+            break;
+          case kKeyEnter:
+            is_enter = true;
+            break;
+          case kKeyBackspace:
+            is_backspace = true;
+            break;
+          default:
+            break;
+        }
+        if (is_backspace) {
+          Si32 len = static_cast<Si32>(g_project_name.size());
+          if (len) {
+            g_project_name.replace(len - 1, 1, "");
+          }
+        }
+        if (is_enter) {
+          if (g_project_name.size()) {
+            return true;
+          }
+        }
+        if (ch[0] != '\0') {
+          g_project_name.append(ch);
+        }
+      }
+    }
+    
+    const char *welcome = u8"The Snow Wizard\n\n"
+    "This wizard will create a new Arctic Engine project for you.\n\n"
+    "Enter the project name:  \"%s\"\n\n"
+    "You may use only latin letters, numbers and underscores\n"
+    "Press ESC to leave the Snow Wizard";
+    char text[1024];
+    snprintf(text, sizeof(text), welcome, g_project_name.c_str());
+    g_font.Draw(text, 32, ScreenSize().y - 32, kTextOriginTop);
+    ShowFrame();
+  }
+  return false;
 }
-void Init() {
-	g_blocks[1].Load("data/block_1.tga");
-	g_blocks[2].Load("data/block_2.tga");
-	ResizeScreen(800, 500);
-	StartNewTetramino();
-	g_prev_time = Time();
-  g_score = 0;
-  g_font.Load("data/arctic_one_bmf.fnt");
+
+bool IsProjectNameOk() {
+  struct stat info;
+  if (stat(g_project_name.c_str(), &info) != 0) {
+    return true;
+  } else if( info.st_mode & S_IFDIR ) {
+    return false;
+  } else {
+    return false;
+  }
 }
-bool IsPositionOk(Si32 test_x, Si32 test_y, Si32 test_orientation) {
-	for (Si32 y = 0; y < 5; ++y) {
-		for (Si32 x = 0; x < 5; ++x) {
-			if (g_current[test_orientation][y][x]) {
-				if (x + test_x < 0 || x + test_x > 7 || y + test_y > 15
-					    || g_field[y + test_y][x + test_x]) {
-					return false;
-				}
-			}
-		}
-	}
-	return true;
+
+bool ShowProgress() {
+  Si32 step = 0;
+  char text[1 << 20];
+  while (!IsKeyDownward(kKeyEscape)) {
+    switch (step) {
+      case 1:
+        if (IsProjectNameOk()) {
+          g_progress.append(u8"Directory name is OK\n");
+        } else {
+          g_progress.append(u8"A directory named \"");
+          g_progress.append(g_project_name);
+          g_progress.append(u8"\" already exists. Use another name.\n");
+          step = 100500;
+        }
+        break;
+      default:
+        break;
+    }
+    step++;
+    
+    UpdateResolution();
+    Clear();
+    const char *welcome = u8"The Snow Wizard\n\n"
+    "Creating project \"%s\"\n\n"
+    "%s\n\n"
+    "Press ESC to leave the Snow Wizard";
+    
+    snprintf(text, sizeof(text), welcome,
+        g_project_name.c_str(), g_progress.c_str());
+    g_font.Draw(text, 32, ScreenSize().y - 32, kTextOriginTop);
+    ShowFrame();
+  }
+  return false;
 }
-void LockTetramino() {
-	for (Si32 y = 0; y < 5; ++y) {
-		for (Si32 x = 0; x < 5; ++x) {
-			if (g_current[g_current_orientation][y][x]) {
-				g_field[y + g_current_y][x + g_current_x] = 2;
-			}
-		}
-	}
-	bool do_continue = true;
-	while (do_continue) {
-		do_continue = false;
-		for (Si32 y = 15; y >= 0; --y) {
-			bool is_full_line = true;
-			for (Si32 x = 0; x < 8; ++x) {
-				if (!g_field[y][x]) {
-					is_full_line = false;
-					break;
-				}
-			}
-			if (is_full_line) {
-        g_score += 100;
-				do_continue = true;
-				for (Si32 y2 = y; y2 > 0; --y2) {
-					for (Si32 x = 0; x < 8; ++x) {
-						g_field[y2][x] = g_field[y2 - 1][x];
-					}
-				}
-			}
-		}
-	}
-}
-void Update() {
-	double time = Time();
-	if (IsKeyDownward(kKeyLeft) || IsKeyDownward("a")) {
-		g_dx = -1;
-	} else if (IsKeyDownward(kKeyRight) || IsKeyDownward("d")) {
-		g_dx = 1;
-	}
-	g_rotate = g_rotate || IsKeyDownward(kKeyUp) || IsKeyDownward("w") || IsKeyDownward(" ");
-	g_drop = g_drop || IsKeyDownward(kKeyDown) || IsKeyDownward("s");
-	if (IsKeyDownward("c")) {
-		ClearField();
-		StartNewTetramino();
-		return;
-	}
-	if (time - g_prev_time < 0.5) {
-		return;
-	}
-	g_prev_time = time;
-	g_dy = 1;
-	if (g_dx && IsPositionOk(g_current_x + g_dx, g_current_y, g_current_orientation)) {
-		g_current_x += g_dx;
-		g_dy = 0;
-	} else {
-		g_dx = 0;
-	}
-	if (g_rotate) {
-		if (IsPositionOk(g_current_x, g_current_y, (g_current_orientation + 1) % 4)) {
-			g_current_orientation = (g_current_orientation + 1) % 4;
-			g_dy = 0;
-		}
-	}
-	bool is_lock = false;
-	if (IsPositionOk(g_current_x, g_current_y + (g_dy ? 1 : 0), g_current_orientation)) {
-		g_current_y += (g_dy ? 1 : 0);
-	} else {
-		is_lock = (g_dx == 0);
-	}
-	if (g_drop) {
-		while (IsPositionOk(g_current_x, g_current_y + 1, g_current_orientation)) {
-			g_current_y++;
-		}
-	}
-	g_dx = 0;
-	g_dy = 0;
-	g_drop = 0;
-	g_rotate = 0;
-	if (is_lock) {
-		LockTetramino();
-		StartNewTetramino();
-	}
-}
-void Render() {
-	Clear();
-  Si32 x_offset = (ScreenSize().x - 25 * 8) / 2;
-	for (Si32 y = 0; y < 16; ++y) {
-		for (Si32 x = 0; x < 8; ++x) {
-			g_blocks[g_field[y][x]].Draw(x_offset + x * 25, (15 - y) * 25);
-		}
-	}
-	for (Si32 y = 0; y < 5; ++y) {
-		for (Si32 x = 0; x < 5; ++x) {
-			g_blocks[g_current[g_current_orientation][y][x]].Draw(
-        x_offset + (x + g_current_x) * 25, (15 - y - g_current_y) * 25);
-		}
-	}
-  char score[128];
-  snprintf(score, sizeof(score), u8"Score: %d", g_score);
-  g_font.Draw(score, 0, ScreenSize().y, kTextOriginTop);
-	ShowFrame();
-}
+
 void EasyMain() {
-	Init();
-	while (!IsKeyDownward(kKeyEscape)) {
-		Update();
-		Render();
-	}
+  g_font.Load("data/arctic_one_bmf.fnt");
+  if (!GetProjectName()) {
+    return;
+  }
+  if (!ShowProgress()) {
+    return;
+  }
 }
