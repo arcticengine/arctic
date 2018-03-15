fae8f7cf1512a9a5dd31f0ba5793323ab25095b9
Huldra <arcticengine@gmail.com>
2018-03-16 02:45:22 +0300

    Make the license look the same way

--- engine/arctic_platform_windows.cpp
+++ engine/arctic_platform_windows.cpp
@@ -1,10 +1,10 @@
-// The MIT License(MIT)
+// The MIT License (MIT)
 //
-// Copyright 2015 - 2016 Inigo Quilez
-// Copyright 2017 - 2018 Huldra
+// Copyright (c) 2015 - 2016 Inigo Quilez
+// Copyright (c) 2017 - 2018 Huldra
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
+// of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
 // copies of the Software, and to permit persons to whom the Software is
--- engine/opengl.h
+++ engine/opengl.h
@@ -1,13 +1,13 @@
-// The MIT License(MIT)
+// The MIT License (MIT)
 //
-// Copyright 2016 - 2017 Huldra
+// Copyright (c) 2016 - 2017 Huldra
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
+// of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
 // copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
+// furnished to do so, subject to the following conditions:
 //
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
