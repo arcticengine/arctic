fb7a72910a83e0c58d0e418c6ed9b7eede0962ed
Huldra <arcticengine@gmail.com>
2020-07-18 14:37:06 +0300

    Add update script for sample projects

---
+++ update_with_wizard.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+DIR="`pwd`"
+open ./wizard/wizard.app --args update "${DIR}/antarctica_pyramids"
+open ./wizard/wizard.app --args update "${DIR}/filetest"
+open ./wizard/wizard.app --args update "${DIR}/tests"
+open ./wizard/wizard.app --args update "${DIR}/wizard"
