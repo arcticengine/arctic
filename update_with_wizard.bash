ec8eae667704b5bd2cc0949bf2b1d6dd19ec1c7d
Huldra <arcticengine@gmail.com>
2020-07-18 22:36:15 +0300

    Fix project update script

--- update_with_wizard.bash
+++ update_with_wizard.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR="`pwd`"
 open ./wizard/wizard.app --args update "${DIR}/antarctica_pyramids"
+sleep 3
 open ./wizard/wizard.app --args update "${DIR}/filetest"
+sleep 3
 open ./wizard/wizard.app --args update "${DIR}/tests"
+sleep 3
 open ./wizard/wizard.app --args update "${DIR}/wizard"
