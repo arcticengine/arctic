133d9db3abb2b349889725721f750ada55a133e0
Huldra <arcticengine@gmail.com>
2020-08-02 21:38:00 +0000

    Fuzz csv and tga loaders

---
+++ fuzz_tests/fuzz_tga.cpp
@@ -0,0 +1,26 @@
+#include <engine/easy_sprite.h>
+#include <engine/arctic_types.h>
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <setjmp.h>
+
+jmp_buf arctic_jmp_env;
+using namespace arctic;
+using namespace easy;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  int val = setjmp(arctic_jmp_env);
+  if(val == 1337) {
+    return 0;
+  }
+  Sprite s;
+  s.LoadFromData(data, size, "a.tga");
+  return 0;
+}
+
+
