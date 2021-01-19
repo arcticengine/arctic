a10daa7737e6f4fc6af683daccdcb18d2473a350
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2021-01-19 17:14:32 +0300

    Update copyrights

--- engine/easy_gl_sprite_instance.cpp
+++ engine/easy_gl_sprite_instance.cpp
@@ -5,6 +5,7 @@
 //
 // Copyright (c) 2015 - 2017 Inigo Quilez
 // Copyright (c) 2017 - 2020 Huldra
+// Copyright (c) 2021 Vlad2001_MFS
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
