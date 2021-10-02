0d49e10d21eac8704da55ea1e05e06dd54c2aee6
Huldra <arcticengine@gmail.com>
2021-10-02 18:32:24 +0300

    Implement sound compressor

--- engine/arctic_platform_pi_sound.cpp
+++ engine/arctic_platform_pi_sound.cpp
@@ -3,7 +3,7 @@
 
 // The MIT License (MIT)
 //
-// Copyright (c) 2017 - 2019 Huldra
+// Copyright (c) 2017 - 2021 Huldra
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -123,7 +123,7 @@ static unsigned int g_period_time_us = 10000;
 
 struct async_private_data {
   std::vector<Si16> samples;
-  std::vector<Si32> mix;
+  std::vector<float> mix;
   std::vector<Si16> tmp;
   snd_async_handler_t *ahandler = nullptr;
   snd_pcm_t *handle = nullptr;
@@ -136,95 +136,19 @@ static async_private_data g_data;
 
 void MixSound() {
   async_private_data *data = &g_data;
-  Si32 *mix_l = &data->mix[0];
-  Si32 *mix_r = &data->mix[1];
+  float *mix_l = &data->mix[0];
+  float *mix_r = &data->mix[1];
   Si32 mix_stride = 2;
-
   Si32 buffer_samples_per_channel = data->period_size;
 
-  float master_volume = 1.0f;
-  {
-    g_sound_mixer_state.InputTasksToMixerThread();
-    master_volume = g_sound_mixer_state.master_volume.load();
-    if (g_sound_mixer_state.buffers.empty()) {
-      Si32 mix_idx = 0;
-      for (Ui32 i = 0; i < buffer_samples_per_channel; ++i) {
-        mix_l[mix_idx] = 0;
-        mix_r[mix_idx] = 0;
-        mix_idx += mix_stride;
-      }
-    }
+  g_sound_mixer_state.MixSound(mix_l, mix_r, mix_stride, buffer_samples_per_channel, data->tmp.data());
 
-    for (Ui32 idx = 0;
-        idx < g_sound_mixer_state.buffers.size(); ++idx) {
-      SoundTask &sound = *g_sound_mixer_state.buffers[idx];
-      
-      if (sound.is_3d) {
-        if (idx == 0) {
-          Si32 mix_idx = 0;
-          for (Ui32 i = 0; i < buffer_samples_per_channel; ++i) {
-            mix_l[mix_idx] = 0;
-            mix_r[mix_idx] = 0;
-            mix_idx += mix_stride;
-          }
-        }
-        bool is_over = true;
-        for (Si32 channel_idx = 0; channel_idx < 2; ++channel_idx) {
-          RenderSound<Si32>(
-              &sound, g_sound_mixer_state.head, channel_idx,
-              (channel_idx == 0 ? mix_l : mix_r), 2, buffer_samples_per_channel, 44100.0,
-              1.f);
-          if (sound.channel_playback_state[channel_idx].play_position * 44100.0 < sound.sound.DurationSamples()) {
-            is_over = false;
-          }
-        }
-        if (is_over) {
-          sound.sound.GetInstance()->DecPlaying();
-          g_sound_mixer_state.ReleaseBufferAt(idx);
-          --idx;
-        }
-      } else {
-        Si32 size = sound.sound.StreamOut(sound.next_position, buffer_samples_per_channel,
-            data->tmp.data(), buffer_samples_per_channel * 2);
-        Si16 *in_data = data->tmp.data();
-        float volume = sound.volume;
-        Si32 mix_idx = 0;
-        if (idx == 0) {
-          for (Si32 i = 0; i < size; ++i) {
-            mix_l[mix_idx] = static_cast<Si32>(static_cast<float>(in_data[i * 2]) * volume);
-            mix_r[mix_idx] = static_cast<Si32>(static_cast<float>(in_data[i * 2 + 1]) * volume);
-            mix_idx += mix_stride;
-          }
-          mix_idx = size * mix_stride;
-          for (Si32 i = size; i < buffer_samples_per_channel; ++i) {
-            mix_l[mix_idx] = 0.f;
-            mix_r[mix_idx] = 0.f;
-            mix_idx += mix_stride;
-          }
-        } else {
-          for (Si32 i = 0; i < size; ++i) {
-            mix_l[mix_idx] += static_cast<Si32>(static_cast<float>(in_data[i * 2]) * volume);
-            mix_r[mix_idx] += static_cast<Si32>(static_cast<float>(in_data[i * 2 + 1]) * volume);
-            mix_idx += mix_stride;
-          }
-        }
-        sound.next_position += size;
-
-        if (sound.next_position == sound.sound.DurationSamples()
-            || size == 0) {
-          sound.sound.GetInstance()->DecPlaying();
-          g_sound_mixer_state.ReleaseBufferAt(idx);
-          --idx;
-        }
-      }
-    }
-  }
-
+  // Convert to 16-bit integer format.
   unsigned char *out_buffer = (unsigned char *)data->samples.data();
   Si32 buffer_samples_total = data->period_size * 2;
   for (Si32 i = 0; i < buffer_samples_total; ++i) {
-    float val = static_cast<float>(data->mix[i]) * master_volume;
-    Si16 res = static_cast<Si16>(std::min(std::max(val, -32767.0f), 32767.0f));
+    Si16 res = static_cast<Si16>(Clamp(
+      data->mix[i], -32767.f, 32767.f));
     out_buffer[i * 2 + 0] = res & 0xff;
     out_buffer[i * 2 + 1] = (res >> 8) & 0xff;
   }
