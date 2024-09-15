b73add9e0c06ddc0f7413a161f5893c9a6b40ad2
Huldra <arcticengine@gmail.com>
2024-09-15 10:51:56 +0300

    Fix duplicate declarations

--- engine/data_reader.h
+++ engine/data_reader.h
@@ -40,57 +40,42 @@ struct DataReader {
   /// @return The 64-bit unsigned integer read
   Ui64 ReadUInt64();
 
-  /// @brief Read a 32-bit unsigned integer from the buffer
-  /// @return The 32-bit unsigned integer read
-  Ui32 ReadUInt32();
-
-  /// @brief Read a 64-bit unsigned integer from the buffer
-  /// @return The 64-bit unsigned integer read
-  Ui64 ReadUInt64();
-  
   /// @brief Read a float from the buffer
   float ReadFloat();
   
   /// @brief Read an array of floats from the buffer
   /// @param dst The destination to read to
   /// @param amount The amount of data to read (in floats)
-  /// @return The amount of data read (in floats)
   void ReadFloatarray2(float *dst, Ui64 amount);
 
   /// @brief Read an array of doubles from the buffer
   /// @param dst The destination to read to
   /// @param amount The amount of data to read (in doubles)
-  /// @return The amount of data read (in doubles)
   void ReadDoublearray2(double *dst, Ui64 amount);
 
   /// @brief Read an array of 64-bit unsigned integers from the buffer
   /// @param dst The destination to read to
   /// @param amount The amount of data to read (in 64-bit unsigned integers)
-  /// @return The amount of data read (in 64-bit unsigned integers)
   void ReadUInt64array(Ui64 *dst, Ui64 amount);
   
   /// @brief Read an array of 32-bit unsigned integers from the buffer
   /// @param dst The destination to read to
   /// @param amount The amount of data to read (in 32-bit unsigned integers)
-  /// @return The amount of data read (in 32-bit unsigned integers)
   void ReadUInt32array(Ui32 *dst, Ui64 amount);
 
   /// @brief Read an array of 32-bit unsigned integers from the buffer
   /// @param dst The destination to read to
   /// @param amount The amount of data to read (in 32-bit unsigned integers)
-  /// @return The amount of data read (in 32-bit unsigned integers)
   void ReadUInt32array2(Ui32 *dst, Ui64 amount);
   
   /// @brief Read an array of 16-bit unsigned integers from the buffer
   /// @param dst The destination to read to
   /// @param amount The amount of data to read (in 16-bit unsigned integers)
-  /// @return The amount of data read (in 16-bit unsigned integers)
   void ReadUInt16array(Ui16 *dst, Ui64 amount);
 
   /// @brief Read an array of 8-bit unsigned integers from the buffer
   /// @param dst The destination to read to
   /// @param amount The amount of data to read (in 8-bit unsigned integers)
-  /// @return The amount of data read (in 8-bit unsigned integers)
   void ReadUInt8array(Ui8 *dst, Ui64 amount);
   
 };
