d02ed12148b35c7fa603ffadbb86dc9af95c5b37
Huldra <arcticengine@gmail.com>
2026-02-23 22:00:57 +0300

    Implement IsInBounds for Array2

--- engine/array2.h
+++ engine/array2.h
@@ -112,6 +112,21 @@ template<class T> class Array2 {
   Vec2Si32 Size() const {
     return size_;
   }
+
+  /// @brief Check if the given position is within the bounds of the Array2.
+  /// @param pos The position as a Vec2Si32 to check.
+  /// @return True if pos is inside the bounds of the array, false otherwise.
+  bool IsInBounds(const Vec2Si32 pos) const {
+    return pos.x >= 0 && pos.y >= 0 && pos.x < size_.x && pos.y < size_.y;
+  }
+
+  /// @brief Check if the given coordinates are within the bounds of the Array2.
+  /// @param x The x-coordinate to check.
+  /// @param y The y-coordinate to check.
+  /// @return True if x and y are inside the bounds of the array, false otherwise.
+  bool IsInBounds(const Si32 x, const Si32 y) const {
+    return x >= 0 && y >= 0 && x < size_.x && y < size_.y;
+  }
 };
 
 }  // namespace arctic
