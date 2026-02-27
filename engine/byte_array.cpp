c15b0d7ac03a75722aeca07e82d676ba267b3f95
Huldra <arcticengine@gmail.com>
2026-02-28 00:15:18 +0300

    Remove deprecated files

--- engine/byte_array.cpp
+++
@@ -1,8 +0,0 @@
-// The MIT License (MIT)
-//
-// Copyright (c) 2019 Huldra
-
-#pragma message("Please remove this file (/engine/byte_array.cpp)" \
-    " from your project.")
-
-#include "engine/byte_array.h"
